9a8ae1e981478212640ac9b2ff0151a1bdcaf735
andrewladd <andrewblakeladd@live.com>
2015-11-25 00:45:10 -0500

    updating front end script, better communication, increased modularity

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,49 +1,69 @@
-// $('#submission').on('click', function() {
-$("#nameInput").submit(function(event) {
-	$('#information').hide()
+
+
+$("#nameInput").submit(function (event) {
+    $("#nameInput").blur(); // unselects form
+    $('#information').hide()
+
 	
-	// stop form from submitting normally
-	event.preventDefault();
+    // stop form from submitting normally
+    event.preventDefault();
+    
+    var inputtedName = $('#userName').val();
+	  console.log('beginning request for: ' + inputtedName)
 	
-	var inputtedName = $('#userName').val();
-	console.log('begining request for: ' + inputtedName)
-	
-	//Form validation
-	if (checkUserName(inputtedName)) {
-		$('#loading').show();
+    //Form validation
+    if (checkUserName(inputtedName)) {
+        $('#loading').show();
 		
-		$.ajax({
-			type: 'GET',
-			url: '/user/' + inputtedName,
-			success: function(data) {
-				console.log('success: ' + inputtedName);
-				$('#loading').hide();
-				$('#JSON').text(JSON.stringify(data));
-				
-				var comms = data.comments || '';
-				var subs = data.submissions || ''
-				
-				$('#information').show().text('Successfully gathered '+ comms.length +' comments and '+ subs.length +' submissions from ' + inputtedName)
-			}
-		})
-	} else {
-		//this should be added to an 'error' para instead.
-		$('#information').show().text("The inputted name must conform to reddit's naming standards");
-	}
-});
+        $.ajax({
+            type: 'GET',
+            url: '/user/' + inputtedName,
+            success: function (data) { successOccurs(inputtedName, data) } ,
+            error: function (data) { errorOccurs(inputtedName, data) }
+        })
+    } else {
+        //this should be added to an 'error' para instead.
+        $('#information').show().text("The inputted name must conform to reddit's naming standards");
+    }
+})
+
+
+//what runs upon successful ajax request
+function successOccurs(inputtedName, data) {
+    $('#loading').hide();
+    
+    if (data.error) {
+        $('#information').show().text('Error occurred: ' + data.info)
+    } else {
+        console.log('success: ' + inputtedName);
+        $('.JSON').text(JSON.stringify(data));
+        
+        var comms = (data.comments) ? data.comments : ''; 
+        var subs = data.submissions;// ? data.submissions : ''; 
+        
+        $('#information').show().text('Successfully gathered ' + comms.length + ' comments and ' + subs.length + ' submissions from /u/' + inputtedName)
+    }
+}
+
+function errorOccurs(inputtedName, data) {
+    $('#loading').hide();
+    $('#information').show().text('Unsuccessfully gathered ' + inputtedName + '\'s content for reasons: ' + data)
+    console.log(data)
+}
+
 
 //checks user name against reddit's specs, as well if it exists.
 function checkUserName(name) {
   
-  var flag;
-  var myRegex = /^([a-zA-Z0-9_-]){3,20}$/;
-  
+    var flag;
+    var myRegex = /^([a-zA-Z0-9_-]){3,20}$/;
 
-  if (myRegex.test(name)) {
-    flag = true;
-  } else {
-    flag = false;
-  }
   
-  return flag;
+    if (myRegex.test(name)) {
+        flag = true;
+    } else {
+        flag = false;
+    }
+  
+    return flag;
 }
